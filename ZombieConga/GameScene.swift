f72fb44d97582e1672ceb100bf13be19bd86bc91
Gray <grayzprogrammer@gmail.com>
2022-08-06 20:36:26 -0500

    makes the zombie moving

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -10,6 +10,12 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var count = 0
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
         
@@ -19,10 +25,32 @@ class GameScene: SKScene {
         background.zPosition = -1
         
         // zombie node
-        let zombie = SKSpriteNode(imageNamed: "zombie1")
         zombie.position = CGPoint(x: 400, y: 400)
         
         addChild(background)
         addChild(zombie)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+            count += 1
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        print("\(dt*1000)")
+        
+        //temporary set the limit to the move
+        if count < 100 {
+            move(sprite: zombie, velocity: CGPoint(x: zombieMovePointsPerSec, y: 0))
+        }
+    }
+    
+    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+        print("Amount to move: \(amountToMove)")
+        
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
     }
 }
