375fb4dfba81d13f8875876f3bba64a5eccafd52
Gray <grayzprogrammer@gmail.com>
2022-08-13 13:39:51 -0500

    makes cats wiggling while on screen

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -154,10 +154,14 @@ class GameScene: SKScene {
         cat.setScale(0)
         addChild(cat)
         let appear = SKAction.scale(to: 1.0, duration: 0.5)
-        let wait = SKAction.wait(forDuration: 10.0)
+        cat.zRotation = -π / 16.0
+        let leftWiggle = SKAction.rotate(byAngle: π/8.0, duration: 0.5)
+        let rightWiggle = leftWiggle.reversed()
+        let fullWiggle = SKAction.sequence([leftWiggle, rightWiggle])
+        let wiggleWait = SKAction.repeat(fullWiggle, count: 10)
         let disapper = SKAction.scale(to: 0, duration: 0.5)
         let removeFromParent = SKAction.removeFromParent()
-        let actions = [appear, wait, disapper, removeFromParent]
+        let actions = [appear, wiggleWait, disapper, removeFromParent]
         cat.run(SKAction.sequence(actions))
     }
     
